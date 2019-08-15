a0e6e62459e1498100362a2bd1773db04d0ca39f
Lingfeng <lingfengzheng.88@gmail.com>
2019-08-15 15:43:58 +0200

    version2.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'es-ES'
 
 TIME_ZONE = 'Europe/Madrid'
 
